a24d8abc199966b41b7377e73a72a72e8e2e9f91
Andrew Burns <ErebusBat@gmail.com>
2022-05-27 08:40:33 -0600

    tmux: Add personal startup script
    - Remove 1Password from tmpa as it wont work
      - because zsh sessions are inherited from tmux server and not tmpa

---
+++ bin/executable_ptmux.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# This script is ran from tmux menu to setup sessions
+
+# Authenticate with 1Password so that our ChezMoi status indicator works
+if [[ -x ~/.tmux/op_auth.sh ]]; then
+  eval $(~/.tmux/op_auth.sh)
+fi
+
+sessions=(
+  system
+)
+START_SESS=$1
+[[ -z $START_SESS ]] && START_SESS=system
+
+echo "Starting sessions... will switch to $START_SESS when done."
+for s in $sessions; do
+  tmuxp load -d $s
+  sleep 5
+done
+
+if [[ -n $START_SESS ]]; then
+  echo "Attaching to $START_SESS"
+  # use tmpa script (opposed to `tmuxp load`) so we get fuzzy matching
+  tmpa $START_SESS
+fi
