d2fab38812fbd48ae391b5221d270487db4e14c6
Andrew Burns <andrew.burns@tractionguest.com>
2022-05-27 07:34:33 -0600

    Add bin/executable_tgtmux.zsh

---
+++ bin/executable_tgtmux.zsh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+# $HOME/bin/tgtmux.zsh
+# This script is ran from tmux menu to setup sessions
+
+sessions=(
+  guest-server
+  system
+)
+START_SESS=$1
+[[ -z $START_SESS ]] && START_SESS=guest-server
+
+echo "Starting sessions... will switch to $START_SESS when done."
+for s in $sessions; do
+  tmuxp load -d $s
+  sleep 5
+done
+
+# echo "Starting FetLife VPN Connection"
+# nmcli con up FetLife
+
+# echo "Updating Toggl status"
+# toggl
+
+if [[ -n $START_SESS ]]; then
+  echo "Attaching to $START_SESS"
+  # use tmpa script (opposed to `tmuxp load`) so we get fuzzy matching
+  tmpa $START_SESS
+fi
