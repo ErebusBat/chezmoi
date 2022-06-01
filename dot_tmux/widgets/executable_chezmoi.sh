7795decb0e770465163c70280f9502e142467650
Andrew Burns <andrew.burns@tractionguest.com>
2022-06-01 17:45:36 -0600

    tmux: Make sure that 1Password session is updated for Chezmoi helepr

--- dot_tmux/widgets/executable_chezmoi.sh
+++ dot_tmux/widgets/executable_chezmoi.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+
+# If we have a 1Password helper, then source it
+if [[ -x ~/.tmux/op_auth.sh ]]; then
+  eval $(~/.tmux/op_auth.sh)
+fi
+
 chezmoi verify
 if [[ $? -ne 0 ]]; then
   printf "#[fg=colour1, bg=colour237]CZ╳"
