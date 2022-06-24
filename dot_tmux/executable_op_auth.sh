cd8f0a66143717a0358d75436d76f01d9858a7f9
Andrew Burns <ErebusBat@gmail.com>
2022-06-24 11:14:15 -0600

    Add direct/cron/dartp6 Update dot_tmux/executable_op_auth.sh

--- dot_tmux/executable_op_auth.sh
+++ dot_tmux/executable_op_auth.sh
@@ -1,29 +1,39 @@
 #!/bin/zsh
 SESSION_FILE=/tmp/.op_session.$UID
 
-FORCE=$1
+RO=
 
-if [[ -n $FORCE ]]; then
+if [[ $1 == "1" ]]; then
   if [[ -f $SESSION_FILE ]]; then
     rm -f $SESSION_FILE
   fi
+elif [[ $1 == "RO" ]]; then
+  RO=1
 fi
 
 # If file exists just eval it
-if [[ -f $SESSION_FILE ]]; then
+if [[ -s $SESSION_FILE ]]; then
   cat $SESSION_FILE
+elif [[ -n $RO ]]; then
+  echo "RO Flag specified and session file non existant"
 else
   EC=1
   while [[ $EC -ne 0 ]]; do
+    echo "EC=$EC" >&2
     if [[ -f $SESSION_FILE ]]; then
+      echo "removing $SESSION_FILE" >&2
       rm -f $SESSION_FILE
     fi
 
     op signin -f > $SESSION_FILE
     EC=$?
+    echo "after EC=$EC" >&2
+    cat $SESSION_FILE >&2
+    echo "^^^ file" >&2
     chown $USER $SESSION_FILE
     chmod 400 $SESSION_FILE
   done
+    cat $SESSION_FILE >&2
   cat $SESSION_FILE
 fi
 
