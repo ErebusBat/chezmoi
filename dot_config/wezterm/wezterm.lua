579684208d3587d98dd606c229755e01f79fef99
Andrew Burns <ErebusBat@gmail.com>
2022-12-07 12:11:34 -0700

    Update dot_config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -19,6 +19,7 @@ return {
     --family="Comic Code",
     -- family="Comic Code Ligatures",
     family="ComicCodeLigatures Nerd Font",
+    weight="Regular",
     harfbuzz_features={"calt=1", "clig=1", "liga=1"},
   }),
   -- => == !=
