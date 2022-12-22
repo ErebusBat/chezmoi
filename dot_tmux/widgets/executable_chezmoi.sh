6bf34f7a690ae123016434495df0068a302b6b77
Andrew Burns <andrew.burns@tractionguest.com>
2022-12-22 09:28:27 -0700

    tmux: Fix (round1) themeing to work with base16 shell

--- dot_tmux/widgets/executable_chezmoi.sh
+++ dot_tmux/widgets/executable_chezmoi.sh
@@ -1,14 +1,8 @@
 #!/bin/zsh
-exit 0
-
-# If we have a 1Password helper, then source it
-if [[ -x ~/.tmux/op_auth.sh ]]; then
-  eval $(~/.tmux/op_auth.sh RO)
-fi
 
 chezmoi verify
 if [[ $? -ne 0 ]]; then
-  printf "#[fg=colour1, bg=colour237]CZ╳"
+  printf "#[fg=brightred, bg=default]CZ╳"
 else
-  printf "#[fg=colour22, bg=colour237]CZ♥"
+  printf "#[fg=black, bg=default]CZ♥"
 fi
