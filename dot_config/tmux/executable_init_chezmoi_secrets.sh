8869c9ebdb8590009d533ce46470848466e1e9b0
Andrew Burns <andrew.burns@tractionguest.com>
2022-12-22 15:48:02 -0700

    Add .config/tmux/init_chezmoi_secrets.sh Update .tmux/widgets/chezmoi.sh

---
+++ dot_config/tmux/executable_init_chezmoi_secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Used to init chezmoi secrets store upon tmux invokation
+# so that the chezmoi widget functions
+
+echo "================================================================================"
+echo "Chezmoi Status:"
+chezmoi status
+echo "================================================================================"
--- dot_tmux/widgets/executable_chezmoi.sh
+++ dot_tmux/widgets/executable_chezmoi.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
+# Testing line, for color
+# printf "#[fg=brightblack, bg=default]&"
+
 chezmoi verify
 if [[ $? -ne 0 ]]; then
   printf "#[fg=brightred, bg=default]CZ╳"
 else
-  printf "#[fg=black, bg=default]CZ♥"
+  printf "#[fg=brightblack, bg=default]CZ♥"
 fi
