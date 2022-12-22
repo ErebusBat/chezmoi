5dc696216afd964b2fdf9ec5c7d4f0973a8c7d9d
Andrew Burns <andrew.burns@tractionguest.com>
2022-12-22 08:44:08 -0700

    Remove dot_config/zsh-hooks/base16.zsh Add dot_config/zsh-hooks/executable_base16.zsh

---
+++ dot_config/zsh-hooks/executable_base16.zsh
@@ -0,0 +1,14 @@
+# https://github.com/tinted-theming/base16-shell
+if [[ -f "$HOME/.config/base16-shell/base16-shell.plugin.zsh" ]]; then
+  #
+  # export BASE16_THEME_DEFAULT=darktooth
+  # export BASE16_THEME_DEFAULT=solarized-light
+
+  # export BASE16_THEME_DEFAULT=gruvbox-light-hard
+  # export BASE16_THEME_DEFAULT=ocean
+  # export BASE16_THEME_DEFAULT=dracula
+  export BASE16_THEME_DEFAULT=darkviolet
+
+  source "$HOME/.config/base16-shell/base16-shell.plugin.zsh"
+fi
+
