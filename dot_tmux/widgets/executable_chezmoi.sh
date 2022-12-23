236f9b4e2b9ee50a93181a14df0aec5f094548b0
Andrew Burns <ErebusBat@gmail.com>
2022-12-23 12:06:14 -0700

    Update dot_tmux/widgets/executable_chezmoi.sh

--- dot_tmux/widgets/executable_chezmoi.sh
+++ dot_tmux/widgets/executable_chezmoi.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+# Test to see if we should not run (i.e. for power saving)
+if [[ -f /tmp/lowpower ]]; then
+  exit 0
+fi
+
 # Testing line, for color
 # printf "#[fg=brightblack, bg=default]&"
 
