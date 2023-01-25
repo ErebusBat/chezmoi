c7661b1218319b771dc5a142a863c109e0a50f59
Andrew Burns <andrew.burns@signinsolutions.com>
2023-01-24 17:05:26 -0700

    Update .config/tmux/tmux-status.conf Update .config/tmux/widgets/spotify.sh

--- dot_config/tmux/widgets/executable_spotify.sh
+++ dot_config/tmux/widgets/executable_spotify.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
 NO_PLAY_INDICATOR=1
-SONG_LIMIT=999
+
+TMUX_RIGHT_WIDTH=55 # This should match tmux status-right-length
+# tmux right status is 25 wide w/o song
+(( SONG_LIMIT = $TMUX_RIGHT_WIDTH - 25 ))
 
 # Test to see if we should not run (i.e. for power saving)
 if [[ -f /tmp/lowpower ]]; then
@@ -15,11 +18,21 @@ fi
 # Testing line, for color
 # printf "#[fg=brightblack, bg=default]&"
 
+# Figure out what is actually running
+nowplaying=$(gospt nowplaying)
+if [[ $? -ne 0 ]]; then
+  # printf "#[fg=brightred]gospt err"
+  exit 0
+fi
+if [[ $SONG_LIMIT -gt 0 ]]; then
+  nowplaying=$(echo "${nowplaying}" | cut -c1-$SONG_LIMIT)
+  # nowplaying="${nowplaying}┅"
+fi
+
 # Seperator character + coloring for actual song
 printf "#[fg=brightblack, bg=default]|#[fg=blue]"
 
-nowplaying=$(gospt nowplaying)
-
+# Actual output
 if [[ $NO_PLAY_INDICATOR -eq 1 ]]; then
   printf "$(echo "${nowplaying}" | sed -E 's/^[⏸▶ ]+//g')"
 else
