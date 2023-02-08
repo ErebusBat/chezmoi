c7cd4070d61168fd88baba6b6d3a018a7d26bd49
Andrew Burns <andrew.burns@signinsolutions.com>
2023-02-08 11:15:36 -0700

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,8 +7,19 @@ end
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/index.html
 local wezterm = require 'wezterm';
 local hostname = wezterm.hostname()
+local wallpaper_enabled = true
+
+local wallpapers = {
+  wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/slayer_mark_neon.jpg',
+  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_001.jpeg',
+  wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/1ub6r4ns7eopdsol.jpg',
+  wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/pexels-anni-roenkae-2156881.jpg',
+}
+local wallpaper_to_use = wallpapers[math.random(#wallpapers)]
+-- print("Going to use: " .. wallpaper_to_use)
 
 if (hostname == 'MBP-ABURNS') then
+  -- wallpaper_enabled = false
   -- This can fail on linux, and we don't need it there so only call here
   local monitor_count = tablelength(wezterm.gui.screens()["by_name"])
   -- Depends if we are docked or not
@@ -22,39 +33,47 @@ elseif (hostname == 'thelio')  then
     My_font_size = 12
 else
   My_font_size = 12
+  wallpaper_enabled = false
 end
 
-return {
+local config = {
   hide_tab_bar_if_only_one_tab = true,
   default_prog = { "/bin/zsh" },
   set_environment_variables = {
     shell = "/bin/zsh",
   },
 
-  --
-  -- Background
-  --
-  window_background_opacity = 0.5,
-  background = {
+  keys = {
     {
-      source = {
-        File = wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/slayer_mark_neon.jpg',
-        -- File = wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_001.jpeg',
-        -- File = wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/1ub6r4ns7eopdsol.jpg',
-        -- File = wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/pexels-anni-roenkae-2156881.jpg',
-      },
-      -- repeat_x = 'Mirror',
-      horizontal_align = "Center",
-      vertical_align = "Middle",
-      -- height = 'Contain',
-      -- width = 'Contain',
-      hsb = {
-        -- brightness = 0.01, -- Photos
-        brightness = 0.0125, -- Abstract
-      },
-      -- opacity = 0.99,
-    },
+      key = 'F12',
+      mods = 'SUPER|CTRL|SHIFT|ALT',
+      action = wezterm.action.ReloadConfiguration,
+    }
   },
+
+  -- See below for background
+
+  -- window_background_opacity = 0.5,
+  -- background = {
+  --   {
+  --     source = {
+  --       File = wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/slayer_mark_neon.jpg',
+  --       -- File = wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_001.jpeg',
+  --       -- File = wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/1ub6r4ns7eopdsol.jpg',
+  --       -- File = wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/pexels-anni-roenkae-2156881.jpg',
+  --     },
+  --     -- repeat_x = 'Mirror',
+  --     horizontal_align = "Center",
+  --     vertical_align = "Middle",
+  --     -- height = 'Contain',
+  --     -- width = 'Contain',
+  --     hsb = {
+  --       -- brightness = 0.01, -- Photos
+  --       brightness = 0.0125, -- Abstract
+  --     },
+  --     -- opacity = 0.99,
+  --   },
+  -- },
 
   -- Color schme should be overriden by base16 shell settings,
   -- but this provides a good default until that
@@ -83,3 +102,24 @@ return {
     },
   }
 }
+
+--
+-- Background
+--
+if wallpaper_enabled then
+  config["window_background_opacity"] = 0.5
+  config["background"] = {
+    {
+      source = {
+        File = wallpaper_to_use,
+      },
+      horizontal_align = "Center",
+      vertical_align = "Middle",
+      hsb = {
+        brightness = 0.0125,
+      },
+    }
+  }
+end
+
+return config
