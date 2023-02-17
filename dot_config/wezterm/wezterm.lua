9f8b3c15890a00265e03147c3e8360f8dd5cf7d1
Andrew Burns <andrew.burns@signinsolutions.com>
2023-02-17 14:52:44 -0800

    wezterm: Add wallpaper builder / weights

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,47 +1,46 @@
-function tablelength(T)
-  local count = 0
-  for _ in pairs(T) do count = count + 1 end
-  return count
-end
-
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/index.html
 local wezterm = require 'wezterm';
-local hostname = wezterm.hostname()
-local wallpaper_enabled = true
+-- local hostname = wezterm.hostname()
+-- local wallpaper_enabled = true
 local my_font_size = 14
 
 --
 -- Randomized Wallpaper
 --
-local wallpapers = {
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/1ub6r4ns7eopdsol.jpg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/pexels-anni-roenkae-2156881.jpg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/doom-doom-slayer-4k-xm.jpg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/doom-ed-slayer-3h-1800x1169.jpg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/doom-vfr-5k-sg-1800x1169.jpg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/slayer_mark_neon.jpg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_001.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_002.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_003.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_004.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_lights_01.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_lights_02.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/dk_signs.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/ny22_001.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/ny22_002.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20220918a.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230208.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230216.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ifly.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/linux_001.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/linux_002.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/van01.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/van02.jpeg',
+-- local wallpapers = {
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/1ub6r4ns7eopdsol.jpg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/abstract/pexels-anni-roenkae-2156881.jpg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/doom-doom-slayer-4k-xm.jpg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/doom-ed-slayer-3h-1800x1169.jpg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/doom-vfr-5k-sg-1800x1169.jpg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/doom/slayer_mark_neon.jpg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_001.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_002.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_003.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_hp_004.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_lights_01.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/denver_lights_02.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/dk_signs.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/ny22_001.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/lar/ny22_002.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20220918a.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230208.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230216.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ifly.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/linux_001.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/linux_002.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/van01.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/van02.jpeg',
+--
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230208.jpeg',
+--   wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230216.jpeg',
+-- }
+-- local wallpaper_to_use = wallpapers[math.random(#wallpapers)]
+local wallpaper_info = require('wallpaper')
+-- echo(wallpaper_info)
+local wallpaper_to_use = wallpaper_info.File
+local wallpaper_enabled = wallpaper_info.Enabled
 
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230208.jpeg',
-  wezterm.home_dir .. '/.config/wezterm/wallpaper/photos/ah_20230216.jpeg',
-}
-local wallpaper_to_use = wallpapers[math.random(#wallpapers)]
 
 --
 -- Host Specific Settings
