67fb1a94c973d7edee9d46628fbf7d17c87b10df
Andrew Burns <andrew.burns@signinsolutions.com>
2023-02-27 12:52:33 -0700

    Update .config/nvim/lua/plugins/nvim-tree.lua

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -8,6 +8,9 @@ return {
     -- disable netrw at the very start of your init.lua (strongly advised)
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
+
+    -- Because netrw is disabled we need our own :Browse command so that :GBrowse will work
+    vim.cmd [[command! -nargs=1 Browse silent execute '!open' shellescape(<q-args>,1)]]
   end,
   config = function ()
     -- empty setup using defaults
