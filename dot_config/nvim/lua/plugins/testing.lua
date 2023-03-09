abd41f7977af108266a5a6845f83a431dbc8b754
Andrew Burns <andrew.burns@signinsolutions.com>
2023-03-08 18:49:12 -0700

    Update .config/nvim/lua/plugins/testing.lua

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -19,11 +19,17 @@ return {
     -- vim.cmd [[ let test#ruby#rspec#executable = 'clear; bundle exec spring rspec -f d' ]]
     vim.cmd [[ let test#ruby#rspec#executable = 'clear; bundle exec  rspec -f d' ]]
   end,
+  cmd = {
+    'TestNearest',
+    'TestFile',
+    'TestLast',
+    'TestSuite',
+  },
   keys = {
     {'t<C-n>', '<cmd>TestNearest<CR>', desc = '[T]est [N]earest', remap = true },
-    {'t<C-f>', ':TestFile<CR>', desc = '[T]est [F]ile', remap = true },
-    {'t<C-l>', ':TestLast<CR>', desc = '[T]est [L]ast', remap = true },
-    {'t<C-s>', ':TestSuite<CR>', desc = '[T]est [S]uite', remap = true },
+    {'t<C-f>', '<cmd>TestFile<CR>', desc = '[T]est [F]ile', remap = true },
+    {'t<C-l>', '<cmd>TestLast<CR>', desc = '[T]est [L]ast', remap = true },
+    {'t<C-s>', '<cmd>TestSuite<CR>', desc = '[T]est [S]uite', remap = true },
 
     -- Leader versions which will start the test (using maps above) then switch back to
     -- code pane (via <leader><leader> shortcut).  Meant to be used with harpoon terminal
