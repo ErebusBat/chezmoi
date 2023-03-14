cbd29aa51bf0544358beed77b27e313b1951f968
Andrew Burns <andrew.burns@signinsolutions.com>
2023-03-14 09:23:05 -0600

    Update .chezmoiremove Remove .config/tmux/init_chezmoi_secrets.sh

--- dot_config/tmux/executable_init_chezmoi_secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Used to init chezmoi secrets store upon tmux invokation
-# so that the chezmoi widget functions
-
-echo "================================================================================"
-echo "Dotfiles Status:"
-chezmoi status
-echo "================================================================================"
