fa819b5cf70222322c74ef643bf36680702a549f
Andrew Burns <andrew.burns@signinsolutions.com>
2023-04-04 11:31:34 -0600

    Update .config/nvim/lua/custom/after_plugin.lua

--- dot_config/nvim/lua/custom/literal_after_plugin.lua
+++ dot_config/nvim/lua/custom/literal_after_plugin.lua
@@ -4,6 +4,7 @@
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume', noremap = true })
