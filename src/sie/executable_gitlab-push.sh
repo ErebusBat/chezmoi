d27b6d29aa55253a59d09842b79d3fd38c6d5bdc
Andrew Burns <andrew.burns@signinsolutions.com>
2023-08-17 11:17:36 -0600

    Update .chezmoiignore Add src/sie/gitlab-push.sh

---
+++ src/sie/executable_gitlab-push.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+ROOT_PATH=~/src/sie
+PROJECTS=(
+  server
+  frontend
+)
+
+echo "[$(date)] Starting..."
+for proj in $PROJECTS; do
+  full_path=$ROOT_PATH/$proj
+  if [[ ! -d $full_path ]]; then
+    echo "***ERROR: Project path not found: $full_path"
+    continue
+  fi
+  cd $full_path
+
+  echo "[$(date)] Pushing $proj to bat-gitlab"
+  git push bat-gitlab --all 2>&1
+  git push bat-gitlab --tags 2>&1
+done
+echo "[$(date)] Done."
