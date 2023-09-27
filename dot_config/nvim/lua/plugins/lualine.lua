e204277143aab0be16556cb12e70b38239c92c8b
Andrew Burns <andrew.burns@signinsolutions.com>
2023-09-27 14:55:25 -0600

    Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,8 @@
 return {
   'nvim-lualine/lualine.nvim', -- Fancier statusline
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
   config = function()
     require('lualine').setup {
       options = {
