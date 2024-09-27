1d905828492647355dbf2a725d8d2470a5313527
Andrew Burns <andrew.burns@signinsolutions.com>
2024-09-27 10:40:02 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/theme-init.lua

--- dot_config/nvim/lua/custom/theme-init.lua
+++ dot_config/nvim/lua/custom/theme-init.lua
@@ -1,27 +1,3 @@
---------------------------------------------------------------------------------
--- Theme INIT
---
--- This file is included BEFORE any plugins are loaded.  Its purpose is to set
--- variables that will be used _during_ the plugins configuration
--- See lua/custom/theme-after-plugin.lua for code that will run after the vim
--- environment is up and running
---------------------------------------------------------------------------------
-
-
-vim.g.x_darkmode = true
--- vim.g.x_darkmode = false
-
--- Specific Logic
-if vim.g.x_darkmode then
-  -- vim.g.x_vim_colorscheme = 'base16-catppuccin'
-  -- vim.g.x_lualine_theme = 'base16'
-  vim.g.x_vim_colorscheme = 'catppuccin-mocha'
-  vim.g.x_lualine_theme = vim.g.x_vim_colorscheme
-else
-  -- catppuccin catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-  vim.g.x_vim_colorscheme = 'catppuccin-latte'
-  vim.g.x_lualine_theme = vim.g.x_vim_colorscheme
-end
-
--- See lua/custom/theme-after-plugin.lua for code that runs after plugins are up and running
-
+-- THEME IS MANAGED BY RRethy/nvim-base16
+-- Run base16_XXX commands in terminal (and reload vim) to change theme
+-- set defaults in lua/plugins/base16.lua
