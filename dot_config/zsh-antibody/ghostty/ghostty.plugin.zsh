8bfc15fab90ab3a0013bbe2a5e2a717f062d168b
Andrew Burns <andrew.burns@signinsolutions.com>
2025-01-06 12:32:20 -0700

    Update Brewfile Add .config/zsh-antibody/ghostty/ghostty.plugin.zsh Add .config/zsh-antibody/python/python.plugin.zsh

---
+++ dot_config/zsh-antibody/ghostty/ghostty.plugin.zsh
@@ -0,0 +1,3 @@
+if [[ ! -d $GHOSTTY_RESOURCES_DIR ]]; then return 0; fi
+
+source ${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty-integration
---
+++ dot_config/zsh-antibody/python/python.plugin.zsh
@@ -0,0 +1,3 @@
+if [[ -d /opt/homebrew/opt/python@3.13/libexec/bin ]]; then
+  export PATH=/opt/homebrew/opt/python@3.13/libexec/bin:$PATH
+fi
