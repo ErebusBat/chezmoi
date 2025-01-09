7ccac44a88e85b5b57124af980c883c9aa03791e
Andrew Burns <andrew.burns@signinsolutions.com>
2025-01-09 11:57:23 -0700

    Update .config/zsh-antibody/python/python.plugin.zsh

--- dot_config/zsh-antibody/python/python.plugin.zsh
+++ dot_config/zsh-antibody/python/python.plugin.zsh
@@ -1,3 +1,7 @@
 if [[ -d /opt/homebrew/opt/python@3.13/libexec/bin ]]; then
   export PATH=/opt/homebrew/opt/python@3.13/libexec/bin:$PATH
 fi
+
+if [[ -d $HOME/.local/bin ]]; then
+  export PATH=$HOME/.local/bin:$PATH
+fi
