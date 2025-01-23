fe7b052d3d42515689fa53029e36f55a0c968826
Andrew Burns <erebusbat@gmail.com>
2025-01-23 11:43:51 -0700

    Add bin/startup.zsh Add bin/tmux-sessions-start.zsh Update direct/warprc_personal

---
+++ bin/executable_startup.zsh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+# vim: set ft=zsh ts=2 sw=2 sts=2 et ai si sta:
+
+# TractionGuest startup script
+if [[ -f ~/.config/tmux/init_chezmoi_secrets.sh ]]; then
+  source ~/.config/tmux/init_chezmoi_secrets.sh
+fi
+
+# Starts apps / sessions after a cold boot
+# Will not launch a terminal as it assumes you will be launching a terminal to execute this script.  This also allows for easily switching terminal emulators.
+
+APPS=(
+  # ~/Applications/Restart\ OrbStack.app
+
+  /Applications/Fantastical.app
+  /Applications/Todoist.app
+  /Applications/Due.app
+  # /Applications/Firefox.app
+  /Applications/Safari.app
+  /Applications/Google\ Chrome.app
+  /Applications/Obsidian.app
+  # /Applications/Slack.app
+
+  /System/Applications/Messages.app
+  /Applications/Messenger.app
+  /Applications/Spotify.app
+  /Applications/Discord.app
+)
+
+# APPS=()
+for app in $APPS; do
+  echo "[$(date)] Launching ${app:r:t}"
+  open -g $app
+done
+
+# Terminal apps - With Options
+# open /Applications/Alacritty.app --args -e dlog-tail 1
+
+# Startup Tmux
+if [[ -x ~/bin/tmux-sessions-start.zsh ]]; then
+  ~/bin/tmux-sessions-start.zsh
+fi
---
+++ bin/executable_tmux-sessions-start.zsh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+# $HOME/bin/tgtmux.zsh
+# This script is ran from tmux menu to setup sessions
+
+if [[ -f ~/.config/tmux/init_chezmoi_secrets.sh ]]; then
+  source ~/.config/tmux/init_chezmoi_secrets.sh
+fi
+
+sessions=(
+  system
+)
+START_SESS=$1
+[[ -z $START_SESS ]] && START_SESS=$sessions[1]
+
+echo "Starting tmux sessions... will switch to $START_SESS when done."
+for s in $sessions; do
+  tmuxp load -d $s
+  sleep 5
+done
+
+# echo "Starting FetLife VPN Connection"
+# nmcli con up FetLife
+
+# echo "Updating Toggl status"
+# toggl
+
+if [[ -n $START_SESS ]]; then
+  echo "Attaching to $START_SESS"
+  # use tmpa script (opposed to `tmuxp load`) so we get fuzzy matching
+  tmpa $START_SESS
+fi
