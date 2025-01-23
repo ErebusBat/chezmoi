69cc46dadf49d3e6b663fbcd556494f0062989be
Andrew Burns <erebusbat@gmail.com>
2025-01-23 14:25:32 -0700

    Add msg about creating setec-astronomy file

--- run_001_ensure.zsh
+++ run_001_ensure.zsh
@@ -3,5 +3,6 @@
 # There is a race condition / personal/server issue with this
 # It just needs to exist or chezmoi will fail
 if [[ ! -f $HOME/.config/zsh-antibody/setec-astronomy/setec-astronomy.plugin.zsh ]] && [[ -d $HOME/.config/zsh-antibody/setec-astronomy ]]; then
+  echo "Creating missing setec-astronomy file... you should only see this once (but may want to re-run chezmoi again)"
   touch $HOME/.config/zsh-antibody/setec-astronomy/setec-astronomy.plugin.zsh
 fi
