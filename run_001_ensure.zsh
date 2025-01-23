ff043aabf9757b58099113f21257a00a644fd8e1
Andrew Burns <erebusbat@gmail.com>
2025-01-23 14:30:13 -0700

    Remove 001_ensure.zsh

--- run_001_ensure.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# There is a race condition / personal/server issue with this
-# It just needs to exist or chezmoi will fail
-if [[ ! -f $HOME/.config/zsh-antibody/setec-astronomy/setec-astronomy.plugin.zsh ]] && [[ -d $HOME/.config/zsh-antibody/setec-astronomy ]]; then
-  echo "Creating missing setec-astronomy file... you should only see this once (but may want to re-run chezmoi again)"
-  touch $HOME/.config/zsh-antibody/setec-astronomy/setec-astronomy.plugin.zsh
-fi
