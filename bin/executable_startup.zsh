620ffadc919080cefd75662504fd70354150efc3
Andrew Burns <erebusbat@gmail.com>
2025-01-26 21:59:52 -0700

    Update bin/startup.zsh

--- bin/executable_startup.zsh
+++ bin/executable_startup.zsh
@@ -1,14 +1,23 @@
 #!/bin/zsh
 # vim: set ft=zsh ts=2 sw=2 sts=2 et ai si sta:
 
-# TractionGuest startup script
-if [[ -f ~/.config/tmux/init_chezmoi_secrets.sh ]]; then
-  source ~/.config/tmux/init_chezmoi_secrets.sh
-fi
+################################################################################
+# SSH Idtentity Setup
+################################################################################
+for key in ~/.ssh/id_*; do
+  if [[ $key =~ '\.pub$' ]]; then
+    continue
+  fi
 
+  ssh-add $key
+done
+echo -n "\n"
+
+################################################################################
+# Application Startup
+################################################################################
 # Starts apps / sessions after a cold boot
 # Will not launch a terminal as it assumes you will be launching a terminal to execute this script.  This also allows for easily switching terminal emulators.
-
 APPS=(
   # ~/Applications/Restart\ OrbStack.app
 
@@ -36,7 +45,9 @@ done
 # Terminal apps - With Options
 # open /Applications/Alacritty.app --args -e dlog-tail 1
 
-# Startup Tmux
+################################################################################
+# TMUX Sessions
+################################################################################
 if [[ -x ~/bin/tmux-sessions-start.zsh ]]; then
   ~/bin/tmux-sessions-start.zsh
 fi
