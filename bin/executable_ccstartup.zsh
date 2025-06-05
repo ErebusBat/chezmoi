4cbd115f1e49d625a0f073d2b1f530194eb73b4a
Andrew Burns <ErebusBat@gmail.com>
2025-06-05 09:28:57 -0600

    Update bin/ccstartup.zsh Remove direct/bin_config.json-Mac.json Add fix_ccam_bin.sh Change attributes of direct/bin_config.json-Mac-31.json

--- bin/executable_ccstartup.zsh
+++ bin/executable_ccstartup.zsh
@@ -32,7 +32,7 @@ APPS=(
   /Applications/Slack.app
   /Applications/1Password.app
   /Applications/Granola.app
-  /Applications/Visual Studio Code.app
+  /Applications/Visual\ Studio\ Code.app
 
   # /Applications/Google\ Chrome.app
   # See Below for opening Chrome with the correct profiles
---
+++ fix_ccam_bin.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+mv -v direct/bin_config.json-Mac-*.json direct/bin_config.json-$(hostname -s).json
