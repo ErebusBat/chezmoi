8f860c4651ee158a56d8a4f463381bfc5dbb3804
Andrew Burns <ErebusBat@gmail.com>
2025-06-05 17:39:37 -0600

    Update Brewfile Add .config/zsh-antibody/ccam/ccam.plugin.zsh Update .config/zsh-antibody/plugins.txt

---
+++ dot_config/zsh-antibody/ccam/ccam.plugin.zsh
@@ -0,0 +1,10 @@
+LIBPQ_BIN=/opt/homebrew/opt/libpq/bin
+if [[ ! -d $LIBPQ_BIN ]]; then
+  return
+fi
+if [[ ! -x $LIBPQ_BIN/psql ]]; then
+  return
+fi
+
+export PATH=$PATH:$LIBPQ_BIN
+echo "--- Added $LIBPQ_BIN to PATH"
