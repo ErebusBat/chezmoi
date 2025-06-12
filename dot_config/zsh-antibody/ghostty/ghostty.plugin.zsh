8d5f35050afc95df04966142afb8b40609abd24e
Andrew Burns <andrew.burns@companycam.com>
2025-06-12 09:25:51 -0600

    Update .config/zsh-antibody/ghostty/ghostty.plugin.zsh

--- dot_config/zsh-antibody/ghostty/ghostty.plugin.zsh
+++ dot_config/zsh-antibody/ghostty/ghostty.plugin.zsh
@@ -1,3 +1,10 @@
+# TERM workaround
+if [[ -n "$SSH_CONNECTION" ]] || [[ -n "$SSH_CLIENT" ]]; then
+  if [[ $TERM == "xterm-ghostty" ]]; then
+    export TERM=xterm-256color
+  fi
+fi
+
 if [[ ! -d $GHOSTTY_RESOURCES_DIR ]]; then return 0; fi
 
 source ${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty-integration
