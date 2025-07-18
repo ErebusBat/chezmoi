f7bb2bf7f2650a71268df74f3965f78d424a1869
Andrew Burns <andrew.burns@companycam.com>
2025-07-18 15:44:20 -0600

    Update .config/zsh-antibody/nodejs/nodejs.plugin.zsh

--- dot_config/zsh-antibody/nodejs/nodejs.plugin.zsh
+++ dot_config/zsh-antibody/nodejs/nodejs.plugin.zsh
@@ -4,4 +4,8 @@ fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Load bash_completion only after compinit is available
+if (( $+functions[compdef] )); then
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
