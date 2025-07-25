a50c5bb0fed9429f716678f827be2d69c0d5308e
Andrew Burns <andrew.burns@companycam.com>
2025-07-25 13:47:46 -0600

    Update .raycast-cmds/daily-log.sh

--- dot_raycast-cmds/executable_daily-log.sh
+++ dot_raycast-cmds/executable_daily-log.sh
@@ -4,15 +4,18 @@
 # @raycast.schemaVersion 1
 # @raycast.title Daily Log
 # @raycast.mode silent
-# change mode to fullOutput for debugging
 
 # Optional parameters:
 # @raycast.icon ✏️
 # @raycast.argument1 { "type": "text", "placeholder": "Entry Text" }
-# @raycast.packageName ErebusBat
+# @raycast.packageName ErebusBat/dlog-ruby
 
 # Documentation:
 # @raycast.description Append Daily Log Entry
-# @raycast.author Andrew Burns
+# @raycast.author ErebusBat
+
+# Be sure that your environment is setup for any tools you may need/want to use
 export PATH=$HOME/bin:$PATH
+
+# This should point to the location of the tools/bin_wrapper.sh script
 ~/bin/dlog $*
