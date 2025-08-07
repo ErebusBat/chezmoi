30d48ac78c220b5619df1a124e06e958e625821b
Andrew Burns <andrew.burns@companycam.com>
2025-08-07 10:25:47 -0600

    Update .chezmoiignore Update .config/dlog/010_ccam.rb Update .config/dlog/200_tool_markdown.rb

--- dot_config/dlog/010_ccam.rb
+++ dot_config/dlog/010_ccam.rb
@@ -4,6 +4,7 @@ add_link_gsub '16MBP', alias: 'CCAM-16MBP', page: 'CompanyCam 16" MacBook Pro'
 add_link_gsub 'PTEAM', alias: 'Platform Team', page: 'CompanyCam Platform Team'
 
 ### CompanyCam - People
+add_link_gsub 'ALISON', page: 'Alison Chan', alias: 'Alison'
 add_link_gsub 'AUSTIN', page: 'Austin Kostelansky', alias: 'Austin'
 add_link_gsub 'COURTNEY', page: 'Courtney White', alias: 'Courtney'
 add_link_gsub 'DUSTIN', page: 'Dustin Fisher', alias: 'Dustin'
