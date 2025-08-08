4e9b46ef7e918941de730f70005430b4899abb11
Andrew Burns <andrew.burns@companycam.com>
2025-08-08 08:58:49 -0600

    Update .config/dlog/000_prefixes.rb Update .config/dlog/001_emojis.rb Update .config/dlog/005_personal.rb Update .config/dlog/010_ccam.rb

--- dot_config/dlog/005_personal.rb
+++ dot_config/dlog/005_personal.rb
@@ -1,13 +1,17 @@
+################################################################################
 ### Computers & Servers
-add_link_gsub 'DARTER', '[[dartp6]]'
-add_link_gsub 'DARTP6', page: 'dartp6'  # [[dartp6|Sys76 Laptop]]
-add_link_gsub 'NAS', page: 'FreeNAS'
-add_link_gsub 'M4MBP', alias: 'm4mbp', page: 'MacBook Pro M4'
-add_link_gsub 'NUC', alias: 'nuc01', page: 'IntelNUC'
-add_link_gsub 'MAZE', alias: 'Maze', page: 'IntelNUC 2 - Maze'
-add_link_gsub 'THELIO', alias: 'Thelio', page: 'System76 Thelio Deskop'
+################################################################################
+add_link_gsub 'M4MBP',    alias: 'm4mbp',      page: 'MacBook Pro M4'
+add_link_gsub 'NUC',      alias: 'nuc01',      page: 'IntelNUC'
+add_link_gsub 'MAZE',     alias: 'Maze',       page: 'IntelNUC 2 - Maze'
+add_link_gsub 'THELIO',   alias: 'Thelio',     page: 'System76 Thelio Deskop'
+add_link_gsub 'DARTP6',                        page: 'dartp6'  # [[dartp6|Sys76 Laptop]]
+add_link_gsub 'DARTER',                        page: 'dartp6'
+add_link_gsub 'NAS',                           page: 'FreeNAS'
 
+################################################################################
 ### Projects / Pages
-add_link_gsub 'DLOG', alias: 'dlog', page: 'Daily Log'
-add_link_gsub 'MDT', alias: '', page: 'Markdown Tool'
-add_link_gsub 'TGMD', alias: 'tgmd', page: 'Jira Mark Down Link Tool'
+################################################################################
+add_link_gsub 'DLOG',  alias: 'dlog',  page: 'Daily Log'
+add_link_gsub 'MDT',   alias: '',      page: 'Markdown Tool'
+add_link_gsub 'TGMD',  alias: 'tgmd',  page: 'Jira Mark Down Link Tool'
