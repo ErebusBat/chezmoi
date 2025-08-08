2a78c8be60118161178b2e7fee8082b694f9ac63
Andrew Burns <andrew.burns@companycam.com>
2025-08-08 08:46:48 -0600

    Update .config/dlog/010_ccam.rb

--- dot_config/dlog/010_ccam.rb
+++ dot_config/dlog/010_ccam.rb
@@ -1,9 +1,13 @@
+################################################################################
 ### CompanyCam - Entities
+################################################################################
 add_link_gsub 'CCAM', page: 'CompanyCam'
 add_link_gsub '16MBP', alias: 'CCAM-16MBP', page: 'CompanyCam 16" MacBook Pro'
 add_link_gsub 'PTEAM', alias: 'Platform Team', page: 'CompanyCam Platform Team'
 
+################################################################################
 ### CompanyCam - People
+################################################################################
 add_link_gsub 'ALISON', page: 'Alison Chan', alias: 'Alison'
 add_link_gsub 'AUSTIN', page: 'Austin Kostelansky', alias: 'Austin'
 add_link_gsub 'COURTNEY', page: 'Courtney White', alias: 'Courtney'
@@ -20,7 +24,25 @@ add_link_gsub 'REID', page: 'Reid Alt', alias: 'Reid'
 add_link_gsub 'SHAUN', page: 'Shaun Garwood', alias: 'Shaun'
 add_link_gsub 'SILVIA', page: 'Silvia Marmol', alias: 'Silvia'
 
+################################################################################
 ### CompanyCam - Projects
+################################################################################
 add_link_gsub 'AMPFF', page: 'Amplitude Feature Flags'
 add_link_gsub 'BLINC', alias: 'Blinc Improvments', page: 'Blinc Improvements Jun 2025'
 add_link_gsub 'CAMJAM', alias: 'CamJam', page: 'CamJam 2025'
+
+################################################################################
+### Slack
+################################################################################
+# Channel shortcuts, these are above the escape gsub so they will be automatically escaped
+add_gsub '#DK',   '#data-kitchen'
+add_gsub '#DB',   '#platform-divebar'
+add_gsub '#BE',   '#backend-engineers'
+add_gsub '#TAIL', '#tod-ai-learned'
+add_gsub '#DEVR', '#dev-random'
+
+# Escape channel names in code blocks so they dont turn into Obsidian tags
+add_gsub /#[-a-z0-9]+/i do |entry, match|
+  dbug "SLACK-CHANNEL: #{match}"
+  "`#{match}`"
+end
