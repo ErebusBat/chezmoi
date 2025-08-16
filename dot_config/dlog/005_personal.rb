df171a5a2f2fac0d6319b3c9c5e813adf7a33207
Andrew Burns <erebusbat@gmail.com>
2025-08-16 00:49:38 -0600

    Update .config/dlog/005_personal.rb

--- dot_config/dlog/005_personal.rb
+++ dot_config/dlog/005_personal.rb
@@ -13,6 +13,7 @@ add_link_gsub 'NAS',                           page: 'FreeNAS'
 ### Projects / Pages
 ################################################################################
 add_link_gsub 'DLOG',  alias: 'dlog',  page: 'Daily Log'
-add_link_gsub 'MDT',   alias: '',      page: 'Markdown Tool'
-add_link_gsub 'TGMD',  alias: 'tgmd',  page: 'Jira Mark Down Link Tool'
 add_link_gsub 'FMJ',   alias: 'FMJ',   page: 'FMJ Discovery'
+add_link_gsub 'MDT',                   page: 'Markdown Tool'
+add_link_gsub 'NXIVM',                 page: 'NXIVM'
+add_link_gsub 'TGMD',  alias: 'tgmd',  page: 'Jira Mark Down Link Tool'
