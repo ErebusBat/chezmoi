95488a806aed39b659c22788eb7269db9dc85484
Andrew Burns <erebusbat@gmail.com>
2025-09-17 09:04:25 -0600

    Update .config/dlog/200_tool_markdown.rb

--- dot_config/dlog/200_tool_markdown.rb
+++ dot_config/dlog/200_tool_markdown.rb
@@ -11,6 +11,24 @@ add_gsub /%%/ do |entry, _match|
   tool_output
 end
 
+# Extract phone numbers and parse them via MDT
+[
+  /\d{7}/,
+  /\d{3}-\d{4}/,
+].each do |phone_rx|
+  add_gsub phone_rx do |entry, match|
+    set_tool_path '~/.raycast-cmds/markdown-tool.sh'
+    next unless has_tool?
+
+    run_tool :with_args, match
+    if tool_error?
+      next "❌ MDT-ERROR: #{entry}"
+    end
+
+    tool_output
+  end
+end
+
 # Extract direct issue links and use MDT to render
 # This MUST go after the %% tool, otherwise it will
 # override the clipboard (because of how MDT is implemented)
