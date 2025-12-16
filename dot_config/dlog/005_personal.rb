7502ed888107717fe4b8220298d38b8ad439f8fd
Andrew Burns <erebusbat@gmail.com>
2025-12-16 09:55:43 -0700

    Update .config/dlog/005_personal.rb Update Library/Application Support/JetBrains/RubyMine2025.2/options/nodejs.xml

--- dot_config/dlog/005_personal.rb
+++ dot_config/dlog/005_personal.rb
@@ -2,6 +2,7 @@
 ### Computers & Servers
 ################################################################################
 add_link_gsub 'M4MBP',    alias: 'm4mbp',      page: 'MacBook Pro M4'
+add_link_gsub 'MBP',      alias: 'm4mbp',      page: 'MacBook Pro M4'
 add_link_gsub 'NUC',      alias: 'nuc01',      page: 'IntelNUC'
 add_link_gsub 'MAZE',     alias: 'Maze',       page: 'IntelNUC 2 - Maze'
 add_link_gsub 'THELIO',   alias: 'Thelio',     page: 'System76 Thelio Deskop'
