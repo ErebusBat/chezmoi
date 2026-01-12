01fb5a6c1153bcc72cc62002d8dde21ae53f8387
Andrew Burns <erebusbat@gmail.com>
2026-01-12 15:11:04 -0700

    Update .config/dlog/010_lightspeed.rb Update .config/wezterm/wezterm.lua Update .ssh/assh.d/home_devices.yml

--- dot_config/dlog/010_lightspeed.rb
+++ dot_config/dlog/010_lightspeed.rb
@@ -1,8 +1,10 @@
 ################################################################################
 ### Entities
 ################################################################################
-add_link_gsub 'LSHQ', alias: 'Lightspeed', page: 'LightspeedHQ'
+add_link_gsub 'LSHQ',  alias: 'Lightspeed', page: 'LightspeedHQ'
+add_link_gsub 'LSMB',  alias: 'LS MBP',     page: 'Lightspeed MacBook Pro M4 Pro'
 
 ################################################################################
 ### People
 ################################################################################
+add_link_gsub 'BARNABY', alias: 'Barnaby', page: 'Barnaby Claydon'
