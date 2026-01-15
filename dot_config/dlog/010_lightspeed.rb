4926f5f6ceb949af459420bf509d36c8e7a5d8e6
Andrew Burns <andrew.burns@lightspeedhq.com>
2026-01-15 08:15:15 -0700

    Update .chezmoiignore Update .config/dlog/010_lightspeed.rb

--- dot_config/dlog/010_lightspeed.rb
+++ dot_config/dlog/010_lightspeed.rb
@@ -10,3 +10,4 @@ add_link_gsub 'LSMB',  alias: 'LS MBP',     page: 'Lightspeed MacBook Pro M4 Pro
 add_link_gsub 'BARNABY', alias: 'Barnaby', page: 'Barnaby Claydon'
 add_link_gsub 'BARNB',   alias: 'Barnaby', page: 'Barnaby Claydon'
 add_link_gsub 'KYLE',    alias: 'Kyle',    page: 'Kyle Oliveira'
+add_link_gsub 'DINESH',  alias: 'Dinesh',  page: 'Dinesh Dinesh'
