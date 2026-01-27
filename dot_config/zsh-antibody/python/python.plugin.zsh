896e57b3b1832e552383e09a53066cc95e886f1a
Andrew Burns <ErebusBat@gmail.com>
2026-01-27 11:15:17 -0700

    Update Brewfile Update .config/zsh-antibody/python/python.plugin.zsh Update Library/Application Support/Claude/claude_desktop_config.json

--- dot_config/zsh-antibody/python/python.plugin.zsh
+++ dot_config/zsh-antibody/python/python.plugin.zsh
@@ -1,3 +1,8 @@
-if [[ -d /opt/homebrew/opt/python@3.13/libexec/bin ]]; then
-  export PATH=/opt/homebrew/opt/python@3.13/libexec/bin:$PATH
-fi
+# if [[ -d /opt/homebrew/opt/python@3.13/libexec/bin ]]; then
+#   export PATH=/opt/homebrew/opt/python@3.13/libexec/bin:$PATH
+# fi
+if [[ ! -x $(which pyenv) ]]; then return 0; fi
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
