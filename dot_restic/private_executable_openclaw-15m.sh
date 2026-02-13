06d166edc340db2c49e6f9d821f9b73703a3bd14
Andrew Burns <ErebusBat@gmail.com>
2026-02-13 13:10:45 -0700

    Update .restic/backup Update .restic/openclaw-15m.sh

--- dot_restic/private_executable_openclaw-15m.sh
+++ dot_restic/private_executable_openclaw-15m.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# This should be done automatically, but if we do this then we can run this script directly
+if [[ -z $RESTIC_REPOSITORY ]]; then
+  source ~/.restic/profile-openclaw.sh
+fi
+
+# Setup failure settings AFTER the above check or we will get an
+# ubnound variable failure on the check
 set -euo pipefail
 
 # restic is provided by mise on dart6p
@@ -6,10 +14,7 @@ if ! command -v restic &> /dev/null; then
     source ~/.config/zsh-antibody/mise/mise.plugin.zsh
 fi
 
-# This should be done automatically, but if we do this then we can run this script directly
-if [[ -z $RESTIC_REPOSITORY ]]; then
-  source ~/.restic/profile-openclaw.sh
-fi
+
 restic backup \
   --exclude-caches --tag=cron \
   ~/src/erebusbat/myserver/batbot ~/.openclaw
