03e6e69bfc21abb66c8ee48f6a67d9cc8a9d7067
Andrew Burns <ErebusBat@gmail.com>
2026-02-14 11:35:06 -0700

    Update .restic/profile-openclaw.sh

--- dot_restic/private_executable_profile-openclaw.sh
+++ dot_restic/private_executable_profile-openclaw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source ~/.restic/.restic-env
+
 # Restic will not expand ~
 export RESTIC_REPOSITORY="sftp://rsyncnet/restic/openclaw"
 export RESTIC_PASSWORD_FILE="$HOME/.restic/.erebusbat.key"
