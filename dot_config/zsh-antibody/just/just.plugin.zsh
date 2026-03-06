364493609d6e2359d0ff728a03cff889a2bf5dac
Andrew Burns <ErebusBat@gmail.com>
2026-03-06 16:42:05 -0700

    just: Moar better

--- dot_config/zsh-antibody/just/just.plugin.zsh
+++ dot_config/zsh-antibody/just/just.plugin.zsh
@@ -4,4 +4,18 @@ fi
 
 alias j='just'
 alias jc='just --choose'
+alias jl='just --list'
 
+# TAB completions
+if [[ -o interactive ]]; then
+  _just_load_completions() {
+    source <(just --completions zsh)
+    if (( $+functions[compdef] )); then
+      compdef _just just j jc
+    fi
+    add-zsh-hook -d precmd _just_load_completions
+    unfunction _just_load_completions
+  }
+  autoload -Uz add-zsh-hook
+  add-zsh-hook precmd _just_load_completions
+fi
