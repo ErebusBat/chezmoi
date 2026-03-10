ff26420dd6d21ae9665b29b76aab22231f721030
Andrew Burns <ErebusBat@gmail.com>
2026-03-10 10:52:37 -0600

    Update .config/zsh-antibody/mise/mise.plugin.zsh

--- dot_config/zsh-antibody/mise/mise.plugin.zsh
+++ dot_config/zsh-antibody/mise/mise.plugin.zsh
@@ -1,3 +1,9 @@
+if [[ -n $MISE_SHELL ]]; then
+  # Mise has already been loaded, don't do it again
+  return
+fi
+
+# Find and load
 if [[ -x /opt/homebrew/bin/mise ]]; then
   eval "$(/opt/homebrew/bin/mise activate)"
 elif [[ -x ~/.local/bin/mise ]]; then
