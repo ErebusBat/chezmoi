8cd62f397ec23423c9f888350f605da43c8e6aa9
Andrew Burns <ErebusBat@gmail.com>
2026-03-10 10:38:59 -0600

    Update .config/zsh-antibody/zoxide/zoxide.plugin.zsh

--- dot_config/zsh-antibody/zoxide/zoxide.plugin.zsh
+++ dot_config/zsh-antibody/zoxide/zoxide.plugin.zsh
@@ -5,5 +5,13 @@ fi
 export _ZO_DATA_DIR=$HOME/.config/zoxide
 eval "$(zoxide init --cmd=z zsh)"
 eval "$(zoxide init --cmd=cd zsh)"
-alias wd=cd
 
+wd() {
+  local delay
+  # RANDOM is 0..32767; normalize to 0..1, scale to 0..3.67, then shift to 0.33..4.00s.
+  # 3.67 = (4.0s - 0.33s)
+  delay=$(printf '%.2f' "$((0.33 + (RANDOM / 32767.0) * 3.67))")
+  printf "🚧 WARNING: Don't use wd anymore! \n  🎮 Penalty: %ss\n" "$delay"
+  sleep "$delay"
+  cd "$@"
+}
