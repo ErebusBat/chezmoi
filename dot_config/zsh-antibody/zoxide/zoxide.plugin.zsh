603027edcf10fb8583f0251d0d91fefb10baf56f
Andrew Burns <ErebusBat@gmail.com>
2026-03-11 09:26:41 -0600

    Update .config/zsh-antibody/zoxide/zoxide.plugin.zsh

--- dot_config/zsh-antibody/zoxide/zoxide.plugin.zsh
+++ dot_config/zsh-antibody/zoxide/zoxide.plugin.zsh
@@ -2,6 +2,7 @@ if ! command -v zoxide >/dev/null 2>&1; then
   return
 fi
 
+export _ZO_ECHO=1
 export _ZO_DATA_DIR=$HOME/.config/zoxide
 eval "$(zoxide init --cmd=z zsh)"
 eval "$(zoxide init --cmd=cd zsh)"
