0239c504a8628e09f4bcface2d706236bd2c5606
Andrew Burns <ErebusBat@gmail.com>
2026-03-16 13:32:20 -0600

    Add .raycast-cmds/wezterm-wallpaper.sh

---
+++ dot_raycast-cmds/executable_wezterm-wallpaper.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Terminal Wallpaper
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌈
+# @raycast.argument1 { "type": "text", "placeholder": "Task Name", "optional": true }
+# @raycast.packageName ErebusBat/wezterm-wallpaper
+
+# Documentation:
+# @raycast.description Switch WezTerm Wallpaper
+# @raycast.author ErebusBat
+
+# Be sure that your environment is setup for any tools you may need/want to use
+export PATH=$HOME/bin:$PATH
+WEZTERM_DIR=$HOME/.config/wezterm
+
+TASK_NAME=$1
+
+if [[ -z $TASK_NAME || $TASK_NAME == "." ]]; then
+  TASK_NAME=sr71
+fi
+
+case "$TASK_NAME" in
+  [Ss]afe)
+    TASK_NAME="disable-ah-common"
+    ;;
+  [Uu]nsafe)
+    TASK_NAME="enable-ah-common"
+    ;;
+esac
+
+if [[ $TASK_NAME != random && $TASK_NAME != set-* ]]; then
+  TASK_NAME="set-$TASK_NAME"
+fi
+
+cd @WEZTERM_DIR && just $TASK_NAME
