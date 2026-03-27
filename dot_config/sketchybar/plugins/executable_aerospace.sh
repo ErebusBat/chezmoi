ad9cfba203c5e4221269aac37c912e3adf37e888
Andrew Burns <ErebusBat@gmail.com>
2026-03-27 07:16:47 -0600

    Update .config/aerospace/aerospace.toml Update .config/sketchybar/sketchybarrc Update .config/sketchybar/justfile Update .config/sketchybar/plugins/aerospace.sh

--- dot_config/sketchybar/plugins/executable_aerospace.sh
+++ dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+CONFIG_DIR="$HOME/.config/sketchybar"
 
 FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
@@ -10,3 +9,14 @@ if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
 else
   sketchybar --set $NAME background.color=0x44FFFFFF label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0
 fi
+
+apps=$(aerospace list-windows --workspace "$1" 2>/dev/null | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+if [ "${apps}" != "" ]; then
+  icon_strip=" "
+  while read -r app; do
+    icon_strip+="$($CONFIG_DIR/plugins/icon_map_fn.sh "$app") "
+  done <<<"${apps}"
+else
+  icon_strip=""
+fi
+sketchybar --set $NAME label="$icon_strip"
