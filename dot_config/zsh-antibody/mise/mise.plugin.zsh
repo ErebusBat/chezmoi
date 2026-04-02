36ec9783f9c0b5fb3b52610805f213e70f3ba707
Andrew Burns <ErebusBat@gmail.com>
2026-04-02 12:39:31 -0600

    Update .config/zsh-antibody/mise/mise.plugin.zsh

--- dot_config/zsh-antibody/mise/mise.plugin.zsh
+++ dot_config/zsh-antibody/mise/mise.plugin.zsh
@@ -8,10 +8,13 @@ if [[ -x /opt/homebrew/bin/mise ]]; then
   eval "$(/opt/homebrew/bin/mise activate)"
 elif [[ -x ~/.local/bin/mise ]]; then
   eval "$(~/.local/bin/mise activate)"
+elif [[ -x /usr/local/bin/mise ]]; then
+  eval "$(/usr/local/bin/mise activate)"
 fi
 
 if [[ -n $MISE_SHELL ]]; then
   # We have mise! Do Things!
 
   alias mr='mise run'
+  alias mx='mise exec'
 fi
