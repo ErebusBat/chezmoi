aaf4d1af04080f3b1eec1ca5140690b294577dde
Andrew Burns <ErebusBat@gmail.com>
2026-04-13 12:25:44 -0600

    Update bin/dlog-tail Update .config/zsh-antibody/docker/docker.plugin.zsh Update .ssh/assh.d/home_devices.yml

--- dot_config/zsh-antibody/docker/docker.plugin.zsh
+++ dot_config/zsh-antibody/docker/docker.plugin.zsh
@@ -9,3 +9,33 @@ fi
 alias dcp='docker compose pull'
 alias dcpud='docker compose pull && docker compose up -d'
 alias dcud='docker compose up -d'
+# alias dvdu='docker volume ls -q | xargs -I {} sh -c '\''echo "$(sudo du -sh /var/lib/docker/volumes/{}/_data | cut -f1)\t {}"'\'''
+
+# Docker Volume NCDU
+alias dvncdu='sudo ncdu /var/lib/docker/volumes'
+
+# Docker Volume Disk Usage
+function dvdu() {
+  docker volume ls -q | while read -r vol; do
+    # 1. Check if the volume has the 'cifs' type in its options
+    # We inspect the JSON format for the .Options.type field
+    mount_type=$(docker volume inspect "$vol" --format '{{ .Options.type }}')
+
+    if [ "$mount_type" = "cifs" ]; then
+      # It is a CIFS mount, set size to CIFS explicitly
+      size="CIFS"
+    else
+      # Standard volume: Show progress, then run du
+      printf "Calculating: %s\r" "$vol"
+
+      # Run du and capture output
+      size=$(sudo du -sh "/var/lib/docker/volumes/${vol}/_data" 2>/dev/null | cut -f1)
+
+      # Clear the progress line
+      printf "\r\033[K"
+    fi
+
+    # 2. Output the final formatted result: SIZE <TAB> VOLUME
+    printf "%s\t%s\n" "$size" "$vol"
+  done
+}
