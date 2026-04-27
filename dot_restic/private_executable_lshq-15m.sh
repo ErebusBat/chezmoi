44c5f6f4478364549e22017d3ec24e3a19a392cd
Andrew Burns <ErebusBat@gmail.com>
2026-04-26 19:37:18 -0600

    Add .restic/common-excludes.lst Update .restic/lshq-15m.sh

--- dot_restic/private_executable_lshq-15m.sh
+++ dot_restic/private_executable_lshq-15m.sh
@@ -4,5 +4,11 @@ set -euo pipefail
 restic backup \
   --tag 15m --tag auto \
   --exclude-caches \
+  --exclude-larger-than 50M \
+  --exclude-file $HOME/.restic/common-excludes.lst \
+  --exclude-file $HOME/.restic/15m-excludes.lst \
+  --exclude "**/.git/**/*" \
   "$HOME/Documents/meetings" \
+  "$HOME/.pi" \
+  "$HOME/src" \
   "$HOME/Library/Application Support/OpenOats"
