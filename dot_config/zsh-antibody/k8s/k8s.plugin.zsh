5039126fc12ba8660e650d4dea619decf5bb73e9
Andrew Burns <ErebusBat@gmail.com>
2026-05-22 18:22:22 -0600

    Update .config/starship.toml Update .config/zsh-antibody/k8s/k8s.plugin.zsh

--- dot_config/zsh-antibody/k8s/k8s.plugin.zsh
+++ dot_config/zsh-antibody/k8s/k8s.plugin.zsh
@@ -2,4 +2,8 @@ if [[ `which kubectl` =~ 'not found' ]]; then return 0; fi
 
 # source <(kubectl completion zsh)
 alias k8=$(which kubectl)
-alias ksunset='kubectl config unset current-context'
+alias k8s=$(which kubectl)
+alias k8sunset='kubectl config unset current-context'
+function k8s-set-ns() {
+  kubectl config set-context --current --namespace=$1
+}
