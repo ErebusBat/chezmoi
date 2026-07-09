e0e684f2187f425818ce54ff5a6e5a23a7ab8217
Andrew Burns <ErebusBat@gmail.com>
2026-07-09 16:30:45 +0000

    Add bin/cmux-notify Update .config/zsh-antibody/cmux/cmux.plugin.zsh

--- dot_config/zsh-antibody/cmux/cmux.plugin.zsh
+++ dot_config/zsh-antibody/cmux/cmux.plugin.zsh
@@ -1,31 +1,9 @@
+# cmux-notify is implemented as a standalone script at ~/bin/cmux-notify,
+# which handles both local and `cmux ssh` remote sessions.
+
 if [[ -z $CMUX_WORKSPACE_ID ]]; then
-  # Define a no-op if we don't have cmux
-  function cmux-notify() {}
   return 0
 fi
 
 alias cssh='cmux ssh'
 alias cmmd='cmux markdown open'
-
-function cmux-notify() {
-  local title="👀 Notification"
-  local body=${1:-Task Completed}
-  local msg=${2:-${PWD:t}/}
-
-  if [[ -z $CMUX_SURFACE_ID ]]; then
-    # Interesting, we had CMUX when we were sourced, but do not right now.
-    return 0
-  fi
-
-  if ! command -v cmux 2>&1 >/dev/null; then
-    # Even curiouser, we had Cmux when we were sourced, and we have a surface ID, but we do not have the command.
-    echo "*** WARN⁉️: cmux not available?!?"
-    return 0
-  fi
-
-  cmux notify \
-    --title "$title" \
-    --subtitle "$msg" \
-    --body "$body" \
-    --surface $CMUX_SURFACE_ID
-}
