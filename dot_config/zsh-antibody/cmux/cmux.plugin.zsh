48ce6131f13c95ecff765c3d78fa7831d08b69f1
Andrew Burns <ErebusBat@gmail.com>
2026-07-17 08:58:01 -0600

    Update .chezmoiignore Update .config/zsh-antibody/cmux/cmux.plugin.zsh Add .ssh/cmux-ssh

--- dot_config/zsh-antibody/cmux/cmux.plugin.zsh
+++ dot_config/zsh-antibody/cmux/cmux.plugin.zsh
@@ -1,3 +1,5 @@
+# vim: set ft=zsh ts=2 sw=2 sts=2 et ai si sta:
+
 # cmux-notify is implemented as a standalone script at ~/bin/cmux-notify,
 # which handles both local and `cmux ssh` remote sessions.
 
@@ -9,11 +11,13 @@ fi
 alias cmmd='cmux markdown open'
 
 function cssh() {
-    if [[ $# -eq 0 ]]; then
-      cmux ssh --help
-    elif [[ $# -eq 1 ]]; then
-      cmux ssh -A --name $1 $1
-    else
-      cmux ssh -A "${@}"
-    fi
+  if [[ -x ~/.ssh/cmux-ssh ]]; then
+    ~/.ssh/cmux-ssh "${@}"
+  elif [[ $# -eq 0 ]]; then
+    cmux ssh --help
+  elif [[ $# -eq 1 ]]; then
+    cmux ssh -A --name $1 $1
+  else
+    cmux ssh -A "${@}"
+  fi
 }
