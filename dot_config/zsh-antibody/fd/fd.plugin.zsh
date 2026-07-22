dce0a6a82eb1e425d69e6d3085d042572b7463d8
Andrew Burns <ErebusBat@gmail.com>
2026-07-22 21:51:49 +0000

    Update .config/zsh-antibody/fd/fd.plugin.zsh

--- dot_config/zsh-antibody/fd/fd.plugin.zsh
+++ dot_config/zsh-antibody/fd/fd.plugin.zsh
@@ -2,20 +2,20 @@
 ### On linux fd is in the fd-find packages and is called fdfind
 ################################################################################
 if [[ $(uname) == "Linux" ]]; then
-  if ! command -v fdfind >/dev/null 2>&1; then
-    return
-  fi
-
-  if [[ -h $HOME/bin/fd ]]; then   # Symlink
-    # fd is already a symlink (-h) don't do anything
-    return
-  elif [[ -f $HOME/bin/fd ]]; then # File
-    echo "***WARN: Found fdfind on system, but ~/bin/fd exists!"
-    return
-  fi
-
-  # Create symlink to fdfind
-  ln -s $(which fdfind) $HOME/bin/fd
+  # if ! command -v fdfind >/dev/null 2>&1; then
+  #   return
+  # fi
+  #
+  # if [[ -h $HOME/bin/fd ]]; then   # Symlink
+  #   # fd is already a symlink (-h) don't do anything
+  #   return
+  # elif [[ -f $HOME/bin/fd ]]; then # File
+  #   echo "***WARN: Found fdfind on system, but ~/bin/fd exists!"
+  #   return
+  # fi
+  #
+  # # Create symlink to fdfind
+  # ln -s $(which fdfind) $HOME/bin/fd
 fi
 
 if ! command -v fd >/dev/null 2>&1; then
