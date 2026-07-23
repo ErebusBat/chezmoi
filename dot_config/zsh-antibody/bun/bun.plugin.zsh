2e0fbb1d3549336c5f15ff555cdfaf75bd54b993
Andrew Burns <ErebusBat@gmail.com>
2026-07-23 12:10:35 -0600

    Update .config/zsh-antibody/bun/bun.plugin.zsh

--- dot_config/zsh-antibody/bun/bun.plugin.zsh
+++ dot_config/zsh-antibody/bun/bun.plugin.zsh
@@ -1,16 +1,21 @@
 # bun completions
-[ -s "/Users/andrew.burns/.bun/_bun" ] || return 0
-if ! (( $+functions[compdef] )); then
-  function compdef() { }
-  source "/Users/andrew.burns/.bun/_bun"
-  unfunction compdef
-else
-  source "/Users/andrew.burns/.bun/_bun"
+export BUN_INSTALL="$HOME/.bun"
+if [[ ! -d $BUN_INSTALL ]]; then
+  unset BUN_INSTALL
+  return 0
+fi
+
+if [ -s "~/.bun/_bun" ]; then
+  if ! (( $+functions[compdef] )); then
+    function compdef() { }
+    source "~/.bun/_bun"
+    unfunction compdef
+  else
+    source "~/.bun/_bun"
+  fi
 fi
 
 # bun
-export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
-
-alias claude-mem='/Users/andrew.burns/.bun/bin/bun "/Users/andrew.burns/.claude/plugins/marketplaces/thedotmack/plugin/scripts/worker-service.cjs"'
+alias claude-mem='~/.bun/bin/bun "~/.claude/plugins/marketplaces/thedotmack/plugin/scripts/worker-service.cjs"'
 
