afa4109e8149f28d93dc021a37ea444849c44bfd
Andrew Burns <ErebusBat@gmail.com>
2026-07-27 10:55:58 -0600

    Update .config/herdr/justfile Update .config/zsh-antibody/herdr/herdr.plugin.zsh

--- dot_config/zsh-antibody/herdr/herdr.plugin.zsh
+++ dot_config/zsh-antibody/herdr/herdr.plugin.zsh
@@ -1,4 +1,35 @@
 if ! command -v herdr 2>&1 >/dev/null; then return 0; fi
 
 alias hr='herdr'
-alias hrr='herdr --remote'
+# alias hrr='herdr --remote'
+function hrr() {
+  local server=$1
+  local title=$2
+
+  case "$title:$server" in
+    ?*:*)
+      # Use the title the user gave us.
+      ;;
+    :outpost*)
+      title="🏰Outpost"
+      ;;
+    :maze*|:nuc02*)
+      title="🌽Maze"
+      ;;
+    :nuc*)
+      title="🍿nuc01"
+      ;;
+    :dormouse*)
+      title="🐭dormouse"
+      ;;
+    *)
+      title=$server
+      ;;
+  esac
+
+  if [[ -n $WEZTERM_PANE ]]; then
+    wezterm cli set-tab-title "$title"
+  fi
+  echo "*** INFO: Connecting herdr session ($title) on $server"
+  herdr --remote $server
+}
