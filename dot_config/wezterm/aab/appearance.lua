2f3e9321527472855322c3cca17165e6556ebe6e
Andrew Burns <ErebusBat@gmail.com>
2026-07-28 09:09:59 -0600

    wezterm: Move tab bar to top

--- dot_config/wezterm/aab/appearance.lua
+++ dot_config/wezterm/aab/appearance.lua
@@ -10,7 +10,7 @@ end
 
 return {
   hide_tab_bar_if_only_one_tab = true,
-  tab_bar_at_bottom = true,
+  tab_bar_at_bottom = false,
   window_decorations = eb_window_decorations,
   window_padding = {
     left = 0,
