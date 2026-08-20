83ae0db4fdfae49a44916a2f88a0798ac68ba363
Andrew Burns <ErebusBat@gmail.com>
2026-08-20 12:51:31 -0600

    Update direct/omp/upserve.yml Update .config/nvim/lazy-lock.json Add .config/nvim/lua/plugins/typescript-tools.lua

---
+++ dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,6 @@
+-- https://github.com/pmizio/typescript-tools.nvim#%EF%B8%8F-configuration
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {},
+}
